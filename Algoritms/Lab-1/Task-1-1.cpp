c0d421527386073cf7aa7972ea0361cf643954fc
AsnDen <asnden@gmail.com>
2025-09-12 09:17:28 +0300

    Split code into functions

--- Algoritms/Lab-1/Task-1-1.cpp
+++ Algoritms/Lab-1/Task-1-1.cpp
@@ -1,46 +1,66 @@
 #include <iostream>
 
-int main()
-{
-	int arr[10];
-	int sm = 0;
-
-	// Filling the array and calculating the sum of nums.
-	for (int i = 0; i < 10; i++) {
-		std::cin >> arr[i];
-		sm += arr[i];
-	}
-	
-	// Showing the array
-	for (int i = 0; i < 10; i++) {
-		std::cout << arr[i] << " ";
-	}
-
-	// Showing the sum
-	std::cout << "\n" << sm << "\n";
-
-	// Sorting
-	bool isSorted = false;
-	int box;
-	while (!isSorted) {
-		isSorted = true;
-		for (int i = 0; i < 9; i++) {
-			if (arr[i] > arr[i + 1]){
-				box = arr[i]; // Use another value to swap nums in array
-				arr[i] = arr[i + 1];
-				arr[i + 1] = box;
-				isSorted = false;
+void bubbleSorting(int arr[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int box = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = box;
 			}
 		}
 	}
+}
 
-	// Showing the minimum num in array
-	std::cout << arr[0] << "\n";
-
-	// Showing the sorted array
-	for (int i = 0; i < 10; i++) {
+void showArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
 		std::cout << arr[i] << " ";
 	}
+}
+
+void fillArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		std::cin >> arr[i];
+	}
+}
+
+int findMinInArray(int arr[], int size) {
+	int min = arr[0];
+	for (int i = 1; i < size; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+int findSumOfArray(int arr[], int size) {
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+int main()
+{
+	const int size = 10;
+	int nums[size];
+
+	std::cout << "Fill array with integers" << std::endl;
+	fillArray(nums, size);
+
+	std::cout<< "Your array is:" << std::endl;
+	showArray(nums, size);
+
+	std::cout << std::endl << "Sum of array is: " << findSumOfArray(nums, size) << std::endl;
+
+	std::cout << "Minimum integer in array is " << findMinInArray(nums, size) << std::endl;
+
+	bubbleSorting(nums, size);
+
+	std::cout << "Sorted array is:" << std::endl;
+	showArray(nums, size);
 
 	return 0;
 }
