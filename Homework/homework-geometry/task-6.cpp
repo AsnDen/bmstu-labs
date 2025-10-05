528df83be07c1900651512ab73d20b8021a3ee4e
AsnDen <asnden@gmail.com>
2025-10-05 20:56:34 +0300

    Improved solution

--- Homework/homework-geometry/task-6.cpp
+++ Homework/homework-geometry/task-6.cpp
@@ -1,44 +1,67 @@
 #include <iostream>
 #include <cmath>
-#include <algorithm>
 
-int rotate(int x1, int y1, int x2, int y2, int x3, int y3) {
-    int opr = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
-    if (opr > 0) return 1;   
-    if (opr < 0) return -1;              
+using namespace std;
+
+double defineDotPosition(double xp, double yp, double x1, double y1, double x2, double y2) {
+    return (x2 - x1) * (yp - y1) - (y2 - y1) * (xp - x1);
 }
 
-
-bool onTheSegment(int x1, int y1, int qx, int qy, int x2, int y2) {
-    return ( (x1 <= qx && qx <= x2) || (x2 <= qx && qx <= x1) ) && ( (y1 <= qy && qy <= y2) || (y2 <= qy && qy <= y1) );
+double dist(double x1, double x2, double y1, double y2) {
+    double dx = x1 - x2;
+    double dy = y1 - y2;
+    return sqrt(dx*dx + dy*dy);
 }
 
-bool crossing(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4) {
-    int o1 = rotate(x1,y1, x2,y2, x3,y3);
-    int o2 = rotate(x1,y1, x2,y2, x4,y4);
-    int o3 = rotate(x3,y3, x4,y4, x1,y1);
-    int o4 = rotate(x3,y3, x4,y4, x2,y2);
-
-    if (o1 != o2 && o3 != o4) return true;
-    if (o1 == 0 && onTheSegment(x1,y1, x3,y3, x2,y2)) return true;
-    if (o2 == 0 && onTheSegment(x1,y1, x4,y4, x2,y2)) return true;
-    if (o3 == 0 && onTheSegment(x3,y3, x1,y1, x4,y4)) return true;
-    if (o4 == 0 && onTheSegment(x3,y3, x2,y2, x4,y4)) return true;
-
-    return false;
-}
 int main() {
     double x1, y1, x2, y2, x3, y3, x4, y4; 
-    std::cin >> x1 >> y1;
-    std::cin >> x2 >> y2;
-    std::cin >> x3 >> y3;
-    std::cin >> x4 >> y4;
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+    cin >> x3 >> y3;
+    cin >> x4 >> y4;
 
-    if (crossing(x1, y1, x2, y2, x3, y3, x4, y4)) {
-        std::cout << "The segments intersect";
+    double cp1 = defineDotPosition(x3, y3, x1, y1, x2, y2);
+    double cp2 = defineDotPosition(x4, y4, x1, y1, x2, y2);
+    double cp3 = defineDotPosition(x1, y1, x3, y3, x4, y4);
+    double cp4 = defineDotPosition(x2, y2, x3, y3, x4, y4);
+
+    bool isInsert = false;
+
+    if ((cp1 * cp2 < 0) && (cp3 * cp4 < 0)) {
+        isInsert = true;
+    } 
+    else if ((cp1 * cp2 == 0) && (cp3 * cp4 == 0)){
+        double AB = dist(x1, y1, x2, y2);
+        double AC = dist(x1, y1, x3, y3);
+        double AD = dist(x1, y1, x4, y4);
+        double BC = dist(x2, y2, x3, y3);
+        double BD = dist(x2, y2, x4, y4);
+        double CD = dist(x3, y3, x4, y4);
+        
+        // Checking if any dot is in the segment
+        // C checking
+        if ((AC + BC - AB) == 0){
+            isInsert = true;
+        }
+        // D checking
+        if ((AD + BD - AB) == 0){
+            isInsert = true;
+        }
+        // A checking
+        if ((AC + AD - CD) == 0){
+            isInsert = true;
+        }
+        // B checking
+        if ((BC + BD - CD) == 0){
+            isInsert = true;
+        }
+    }
+
+    if (isInsert) {
+        cout << "The segments intersect";
     } 
     else {
-        std::cout << "The segments don't intersect";
+        cout << "The segments don't intersect";
     }
 
     return 0;
