8b1c1681ba528a4f84a76c191ec926ef61111497
AsnDen <asnden@gmail.com>
2025-10-11 11:27:30 +0300

    Added solution for task 2

---
+++ Homework/homework-4/task-2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+void showMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (int i = 0; i < matrix.size(); i++){
+        for (int j = 0; j < matrix[i].size(); j++) {
+            std::cout << std::setw(5) << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+std::vector<std::vector<int>> reflectMainDiagonal(std::vector<std::vector<int>> matrix) {
+    std::vector<std::vector<int>> newMatrix = matrix;
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            std::swap(newMatrix[i][j], newMatrix[j][i]);
+        }
+    }
+
+    return newMatrix;
+
+}
+
+bool isRowsSumSame(std::vector<std::vector<int>> matrix) {
+    int n = matrix.size();
+
+    std::vector<int> sums;
+
+    for (int i = 0; i < n; i++) {
+        int sm = 0;
+        for (int j = 0; j < n; j++) {
+            sm += matrix[i][j];
+        }
+        sums.push_back(sm);
+    }
+
+    bool areSame = true;
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (sums[i] != sums[j]) {
+                areSame = false;
+                break;
+            }
+        }
+    }
+
+    return areSame;
+
+}
+
+int main() {
+    int n;
+    std::cout << "How many rows and lines are in the matrix?" << std::endl;
+    std::cin >> n;
+    
+    std::cout << "Fill the matrix" << std::endl;
+    std::vector<std::vector<int>> matrix;
+    
+    matrix.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        matrix[i].resize(n);
+        for (int j = 0; j < n; j++) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    if (isRowsSumSame(matrix) && isRowsSumSame(reflectMainDiagonal(matrix))) {
+        std::cout << "YES" << std::endl;
+    }
+    else {
+        std::cout << "NO" << std::endl;
+    }
+    
+    return 0;
+}
