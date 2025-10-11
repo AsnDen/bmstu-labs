480ffada1eb0a5c9b1a79a88cdf3c4a0370da7df
AsnDen <asnden@gmail.com>
2025-10-11 13:42:36 +0300

    Added solution for task 6

---
+++ Homework/homework-4/task-6.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+void showMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (int i = 0; i < matrix.size(); i++){
+        for (int j = 0; j < matrix[i].size(); j++) {
+            std::cout << std::setw(5) << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+double calc2Determ(std::vector<std::vector<int>> matrix) {
+    return matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1];
+}
+
+int main() {
+    int n = 2;
+    std::vector<std::vector<int>> matrix;
+    std::vector<std::vector<int>> matrixX;
+    std::vector<std::vector<int>> matrixY;
+    
+    matrix.resize(n);
+
+    matrix[0] = {2, 3};
+    matrix[1] = {5, -1};
+
+    matrixX = matrix;
+    matrixX[0][0] = 13;
+    matrixX[1][0] = 9;
+
+    matrixY = matrix;
+    matrixY[0][1] = 13;
+    matrixY[1][1] = 9;
+
+    std::cout << calc2Determ(matrixX) << " " << calc2Determ(matrix) << " " << calc2Determ(matrixY) << std::endl;
+
+    std::cout << "X is " << calc2Determ(matrixX)/calc2Determ(matrix) << std::endl;
+    std::cout << "Y is " << calc2Determ(matrixY)/calc2Determ(matrix) << std::endl;
+
+    return 0;
+}
