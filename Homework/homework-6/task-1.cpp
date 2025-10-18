ec23a2293a71676c47bd3dd31a14012ae96f74a5
AsnDen <asnden@gmail.com>
2025-10-18 23:31:15 +0300

    Added solution for task 1

---
+++ Homework/homework-6/task-1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+
+void stringToLower(char str[]) {
+    int len {int(strlen(str))};
+
+    for (int i = 0; i < len; i++) str[i] = tolower(str[i]);
+}
+
+char* stringRemoveSpaces(char str[]) {
+    int len {int(strlen(str))};
+    char* newStr {new char[100]};
+    int index {0};
+
+    for (int i = 0; i < len; i++) (str[i] != ' ') && (newStr[index++] = str[i]);
+
+    newStr[index] = '\0';
+
+    return newStr;
+}
+
+int main() {
+
+    char inp[100];
+    std::cin.getline(inp, 100);
+
+    char* str {stringRemoveSpaces(inp)};
+    stringToLower(str);
+
+    int len {int(strlen(str))};
+
+    bool isPalindome {true};
+    for (int i = 0; i < len/2; i++) {
+        (str[i] != str[len-i-1]) && (isPalindome = false);
+    }
+
+    isPalindome ? printf("String is a palindome") : printf("String is not a palindrome");
+
+    return 0;
+}
